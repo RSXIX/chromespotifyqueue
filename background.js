dce84062d4498b9a5b491f37e81af77ed893e84d
RSXIX <rohansapra19@gmail.com>
2026-01-05 00:55:04 +0530

    Set up extension frontend but Spotify APIs currently unavailable to use

---
+++ background.js
@@ -0,0 +1,15 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "add-to-playlist",
+    title: "Add to Playlist",
+    contexts: ["selection", "link"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info) => {
+  if (info.linkUrl) {
+    console.log("Clicked link:", info.linkUrl);
+  } else {
+    console.log("Selected text:", info.selectionText);
+  }
+});
